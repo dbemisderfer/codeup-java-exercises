5dc3f2aefee62f1d2a4ed28fd8447697cc22a63e
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-20 14:35:17 -0500

    creation of ConsoleExercises for console io exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,2 +1,72 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
+    public static void main(String[] args) {
+
+
+//////////////////////////////////////
+///////////// Console IO /////////////
+//////////////////////////////////////
+        double pi = 3.14159; // use %.2f for two decimal places
+        System.out.format("The value of pi is approximately %.2f", pi);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter a number: ");
+//        int userInputNumber = scanner.nextInt();
+//        System.out.format("Your number was %s.", userInputNumber);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter three separate words:");
+////        System.out.println("Enter the first word: ");
+//        String userInput1 = scanner.next();
+////        System.out.println("Enter the second word: ");
+//        String userInput2 = scanner.next();
+////        System.out.println("Enter the third word: ");
+//        String userInput3 = scanner.next();
+//        System.out.println(userInput1 + "\n" + userInput2 + "\n" + userInput3);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a brief sentence: ");
+////        String userSentence = scanner.next(); // Doesn't work with more than one word
+//        String userSentence = scanner.nextLine();
+//        System.out.format("Your sentence was: %s", userSentence);
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+///////////////// Attempt as using strings ////////////
+//        System.out.println("Please enter separate length and width values:");
+//        String userLength = scanner.next();
+//        String userWidth = scanner.nextLine();
+//        int numLength = (int) userLength;
+//        int numWidth = (int) userWidth;
+///////////////////////////////////////////////////////
+
+///////////////// Using integers //////////////////////
+//        System.out.println("Please enter a length: ");
+//        int userLength = scanner.nextInt();
+//        System.out.println("Please enter a width: ");
+//        int userWidth = scanner.nextInt();
+///////////////////////////////////////////////////////
+
+///////////////// Using decimals //////////////////////
+//        System.out.println("Please enter a length: ");
+//        double userLength = scanner.nextDouble();
+//        System.out.println("Please enter a width: ");
+//        double userWidth = scanner.nextDouble();
+///////////////////////////////////////////////////////
+
+//        System.out.format("You entered %s and %s\n", userLength, userWidth);
+//
+//        System.out.println("The area is: " + (userLength*userWidth));
+//        System.out.println("The perimeter is: " + (2*userLength + 2*userWidth));
+
+
+    }
+
+
 }
+
+// psvm TAB
+// sout TAB
+// CTRL + SHFT + R to run code
