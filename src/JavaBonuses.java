94b2d9c5a63483cdaf9cbbb95a6c4fdc3b7ef4f7
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-21 12:53:52 -0500

    creation of java bonuses file for extra practice

---
+++ src/JavaBonuses.java
@@ -0,0 +1,16 @@
+public class JavaBonuses {
+    public static void main(String[] args) {
+//        Here are a few bonuses:
+//        1) create three variables, start, end, and sum. Use a loop to find the summation of all the numbers between start and end (inclusive)
+
+
+//        2) write a loop which will output all the prime numbers (evenly divisible by only 1 and themselves) between 10 and 10000
+
+
+//        3) output a table representing the binary representations of the powers of 2
+//        decimal       |       binary
+//        1     |     00000001
+//        2     |     00000010
+//        4     |     00000100 (edited)
+    }
+}
