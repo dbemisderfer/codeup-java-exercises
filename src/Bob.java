eacf719faef3fed3cf286b6cdcbc1834bc7c12d5
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-21 20:54:22 -0500

    creation and completion of Bob exercise from strings lesson

---
+++ src/Bob.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        boolean keepGoing = true;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please start a conversation with Bob: ");
+        do {
+            String userInput = scanner.nextLine();
+            if (userInput.endsWith("?")) {
+                System.out.println();
+                System.out.println("Sure.");
+            } else if (userInput.endsWith("!")) {
+                System.out.println();
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.isEmpty()) {
+                System.out.println();
+                System.out.println("Fine. Be that way!");
+            } else if (userInput.equalsIgnoreCase("x")) {
+                keepGoing = false;
+            } else {
+                System.out.println("Whatever.\n");
+            }
+            System.out.println();
+            System.out.println("Please continue the conversation (or enter 'x' to exit): ");
+        } while (keepGoing);
+    }
+}
