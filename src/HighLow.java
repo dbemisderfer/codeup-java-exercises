7c405a82001ad1f54ad1868fc714d2151a99563b
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-24 18:19:31 -0500

    completion of game development 101 exercise from methods lesson

--- src/HighLow.java
+++ src/HighLow.java
@@ -4,22 +4,31 @@ import java.lang.Math;
 public class HighLow {
     public static void main(String[] args) {
 
-        ///////////////////////////////////////////
+///////////////////////////////////////////
 //    4. Dice Rolling Exercise    /////////
 ///////////////////////////////////////////
 
-        boolean keepGoing = true;
-        do {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Please enter the number of sides you would like for your pair of dice: ");
-            int diceSides = scanner.nextInt();
-            int dice1 = (int) (Math.random()*diceSides + 1);
-            int dice2 = (int) (Math.random()*diceSides + 1);
-            System.out.println("Roll the dice!");
-            String rollDice = scanner.next();
-            System.out.printf("You rolled a %d and a %d%n", dice1, dice2);
-            System.out.println();
-        } while (keepGoing);
+
+//        boolean keepGoing = true;
+//        do {
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.print("Please enter the number of sides you would like for your pair of dice: ");
+////            int diceSides = scanner.nextInt();
+//            int diceSides = getInteger(1, 10);
+//            int dice1 = (int) (Math.random()*diceSides + 1);
+//            int dice2 = (int) (Math.random()*diceSides + 1);
+////            System.out.println("Roll the dice!");
+////            String rollDice = scanner.next();
+//            System.out.println();
+//            System.out.printf("You rolled a %d and a %d%n", dice1, dice2);
+//            System.out.println();
+//            System.out.println("Would you like to continue? [y/n]");
+//            String userResponse = scanner.next();
+//            System.out.println();
+//            if ((userResponse.equalsIgnoreCase("n")) || (userResponse.equalsIgnoreCase("no"))) {
+//                keepGoing = false;
+//            }
+//        } while (keepGoing);
 
 //
 //        random() method returns a random number between 0.0 and 0.9...,
@@ -32,6 +41,37 @@ public class HighLow {
 //    5. Game Development 101    //////////
 ///////////////////////////////////////////
 
+
+        boolean keepGoing = true;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            int randomNumber = (int) (Math.random() * 100 + 1);
+            System.out.println(randomNumber);
+            System.out.println();
+            System.out.println("Guess a number between 1 and 100.");
+             do {
+                int userGuess = getInteger(1, 100);
+                if (userGuess < randomNumber) {
+                    System.out.println("Sorry, the number is higher than that.");
+                }
+                if (userGuess > randomNumber) {
+                    System.out.println("Sorry, the number is lower than that.");
+                }
+                if (userGuess == randomNumber) {
+                    System.out.println("You guessed it!");
+                    System.out.println();
+                    break;
+                }
+            } while (keepGoing);
+
+            System.out.println("Would you like to play again? [y/n]");
+            String userResponse = scanner.next();
+            if (userResponse.equalsIgnoreCase("n") || userResponse.equalsIgnoreCase("no")) {
+                keepGoing = false;
+            }
+        } while (keepGoing);
+
+
     }
 
 /////////////////////////////////////////////////////
@@ -42,8 +82,48 @@ public class HighLow {
 //////////////////////////////////////////////////////
 /////////////////////////////////////////////////////
 
+///////////////////////////////////////////
+//    2. Validate User Input   ////////////
+///////////////////////////////////////////
+
+    public static int getInteger(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.printf("Please enter a number between %d and %d (inclusive): ", min, max);
+
+        if (!scanner.hasNextInt()) {
+            String userString = scanner.next();
+            System.out.println();
+            System.out.printf("Sorry, %s is not a valid entry.%n", userString);
+            System.out.println();
+            System.out.printf("Please enter a number between %d and %d (inclusive): ", min, max);
+            System.out.println();
+            return getInteger(min, max);
+        }
+
+//        if (scanner.next().isEmpty()) {
+//            System.out.println();
+//            System.out.println("Sorry, you must enter something.");
+//            return getInteger(min, max);
+//        }
+
+        int userNumber = scanner.nextInt();
+        System.out.println();
+        if (userNumber < min) {
+            System.out.printf("Sorry, that number is too small. Please stay within the range of %d to %d.%n", min, max);
+            System.out.println();
+            System.out.printf("Please enter a number between %d and %d (inclusive): ", min, max);
+            System.out.println();
+            return getInteger(min, max);
+        } else if (userNumber > max) {
+            System.out.printf("Sorry, that number is too big. Please stay within the range of %d to %d.%n", min, max);
+            System.out.println();
+            System.out.printf("Please enter a number between %d and %d (inclusive): ", min, max);
+            System.out.println();
+            return getInteger(min, max);
+        } else {
+            return userNumber;
+        }
 
 
-
-
+    }
 }
