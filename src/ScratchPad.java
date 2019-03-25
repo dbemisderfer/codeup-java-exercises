f2d2eee704f1d38c2b31c02364ededf100ceaf33
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-24 19:43:04 -0500

    added case for num2 == 0

--- src/ScratchPad.java
+++ src/ScratchPad.java
@@ -39,12 +39,15 @@ public class ScratchPad {
 ////            this.age = studentAge;
 ////        }
 
-        System.out.println(multiplyWithoutStar(7, 9));
-        System.out.println(recursionMultiplication(7, 9));
+        System.out.println(multiplyWithoutStar(3, 1));
+        System.out.println(recursionMultiplication(3, 1));
 
         }
 
     public static long multiplyWithoutStar(int num1, int num2) {
+        if (num2 == 0) {
+            return 0;
+        }
         long result = 0;
         for (int i = 0; i < num2; i++) {
             result += num1;
@@ -54,6 +57,9 @@ public class ScratchPad {
 
 
     public static long recursionMultiplication(int num1, int num2) {
+        if (num2 == 0) {
+            return 0;
+        }
         if (num2 == 1) {
             return num1;
         }
