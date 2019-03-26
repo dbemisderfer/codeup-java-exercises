36b7e05c66795dbde1207870737201fb3ef95cb9
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-25 20:32:55 -0500

    creation and completion of restaurant classes for in-class exercise

--- src/RestaurantDish.java
+++ src/RestaurantDish.java
@@ -1,2 +1,27 @@
 public class RestaurantDish {
+    public int costInCents;
+    public String nameOfDish = "Unnamed dish";
+    public boolean wouldRecommend;
+    public void eat() {
+        System.out.println("Nom, nom, nom!");
+    }
+
+    public static void main(String[] args) {
+        RestaurantDish dish = new RestaurantDish();
+        dish.costInCents = 99;
+//        dish.nameOfDish = "Yummy Yum Sandwich";
+        dish.wouldRecommend = true;
+        dish.eat();
+
+        System.out.printf("%n%d %s %b",   //%b for boolean
+                dish.costInCents,
+                dish.nameOfDish,
+                dish.wouldRecommend);
+//        System.out.println(dish.costInCents);
+//        System.out.println(dish.nameOfDish);
+//        System.out.println(dish.wouldRecommend);
+//        System.out.println(RestaurantDish.eat()); //Only if a static method!!!
+//        System.out.println(dish.eat());
+//        System.out.println(eat());
+    }
 }
--- src/RestaurantTest.java
+++ src/RestaurantTest.java
@@ -1,2 +1,15 @@
 public class RestaurantTest {
+//    public static void main(String[] args) {
+//        RestaurantDish dish = new RestaurantDish();
+//        dish.costInCents = 99;
+//        dish.nameOfDish = "Yummy Yum Sandwich";
+//        dish.wouldRecommend = true;
+//        dish.eat();
+//        System.out.println(dish.costInCents);
+//        System.out.println(dish.nameOfDish);
+//        System.out.println(dish.wouldRecommend);
+////        System.out.println(RestaurantDish.eat()); //Only if a static method!!!
+////        System.out.println(dish.eat());
+//
+//    }
 }
