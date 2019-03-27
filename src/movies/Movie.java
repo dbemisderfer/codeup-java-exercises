d017ab1d135471c229f65aa4be7b2ab5be152ec4
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-27 15:20:39 -0500

    creation and partial completion of movie array application exercise from java arrays lesson

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,31 +1,46 @@
 package movies;
 
 public class Movie {
-    private static String name;
-    private static String category;
+    private String name;
+    private String category;
 
-    public Movie(String movieName, String movieCategory) {
-    this.name = movieName;
-    this.category = movieCategory;
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
     }
 
-    public static String getName() {
+    public String getName() {
         return name;
     }
 
-    public static void setName(String newName) {
-        Movie.name = newName;
+    public void setName(String newName) {
+        this.name = newName;
     }
 
-    public static String getCategory() {
+    public String getCategory() {
         return category;
     }
 
-    public static void setCategory(String newCategory) {
-        Movie.category = newCategory;
+    public void setCategory(String newCategory) {
+        this.category = newCategory;
     }
 
-    public static void main(String[] args) {
+//    public String getMovie(String scifi) {
+//        Movie[] movieArray = MoviesArray.findAll();
+//        for (Movie movie : movieArray) {
+////            System.out.println(movie[0]); // returns memory location
+////            System.out.println(Arrays.toString(movie));
+////            System.out.println(movie.getName());
+////            System.out.println(movie.getCategory());
+//            if (movie.getCategory().equals("scifi")) {
+//                System.out.println(movie.getName());
+//            }
+//        }
+//    }
 
-    }
+//    public static void main(String[] args) {
+//        Movie movie1 = new Movie("Hello", "drama");
+//    }
+
+
 }
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,4 +1,70 @@
 package movies;
 
+import java.util.Arrays;
+
 public class MoviesApplication {
+    public String output;
+
+    public static void getMovieCategory(Movie[] movieArray, int userSelection) {
+
+        if (userSelection == 5) {
+            for (Movie movie : movieArray) {
+                if (movie.getCategory().equals("scifi")) {
+                    System.out.println(movie.getName());
+                }
+            }
+
+        } else if (userSelection == 4) {
+            for (Movie movie : movieArray) {
+                if (movie.getCategory().equals("horror")) {
+                    System.out.println(movie.getName());
+                }
+            }
+        } else if (userSelection == 3){
+            for (Movie movie : movieArray) {
+                if (movie.getCategory().equals("drama")) {
+                    System.out.println(movie.getName());
+                }
+            }
+        } else if (userSelection == 2) {
+            for (Movie movie : movieArray) {
+                if (movie.getCategory().equals("animated")) {
+                    System.out.println(movie.getName());
+                }
+            }
+        } else if (userSelection == 1) {
+            for (Movie movie : movieArray) {
+                System.out.println(movie.getName());
+                }
+        }
+
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(MoviesArray.findAll());
+//        Movie movie2 = new Movie("Goodbye", "drama");
+//        System.out.println(Arrays.toString(MoviesArray.findAll()));
+//        for (Movie movie : MoviesArray.findAll()) {
+//            System.out.println(movie);
+//        }
+//        Movie[] movieArray = MoviesArray.findAll();
+//////        for (int i = 0; i < movieArray.length; i++) {
+//////            System.out.println(movieArray[i].getName());
+//////            System.out.println(movieArray[i].getCategory());
+//////        }
+//        for (Movie movie : movieArray) {
+////            System.out.println(movie[0]); // returns memory location
+////            System.out.println(Arrays.toString(movie));
+//            System.out.println(movie.getName());
+////            System.out.println(movie.getCategory());
+////            if (movie.getCategory().equals("scifi")) {
+////                System.out.println(movie.getName());
+//            }
+//
+//        }
+//        System.out.println(Arrays.toString(movieArray));
+//        System.out.println(Arrays.toString());
+//        System.out.println(getMovieCategory(movieArray, 5));
+//        getMovieCategory(movieArray, 5);
+    }
 }
