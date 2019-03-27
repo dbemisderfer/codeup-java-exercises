c9c770e899f7725182ade1864250bd80ba09cced
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-27 11:52:39 -0500

    creation of movie exercise from arrays lesson and completion of getters, setters, and constructor

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,4 +1,31 @@
 package movies;
 
 public class Movie {
+    private static String name;
+    private static String category;
+
+    public Movie(String movieName, String movieCategory) {
+    this.name = movieName;
+    this.category = movieCategory;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String newName) {
+        Movie.name = newName;
+    }
+
+    public static String getCategory() {
+        return category;
+    }
+
+    public static void setCategory(String newCategory) {
+        Movie.category = newCategory;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
