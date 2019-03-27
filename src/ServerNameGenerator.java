25aeafa2d62b52ed986d93737a6651432145beef
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-27 10:06:46 -0500

    creation and completion of servernamegenerator exercise from arrays lesson

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,51 @@
+import java.lang.reflect.Array;
+
+public class ServerNameGenerator {
+    private static String randomNoun;
+    private static String randomAdj;
+
+    public static String randomElement(String array[]) {
+        int x = (int) (Math.random() * (array.length));
+
+        return array[x];
+    }
+
+//    public static String hyphenatedString(String str1, String str2) {
+//        return
+//    }
+
+    public static void main(String[] args) {
+
+        String[] adjectives = {
+                "bright",
+                "lovely",
+                "moist",
+                "balmy",
+                "wild",
+                "wonderful",
+                "heavenly",
+                "dim",
+                "stupid",
+                "clammy"
+        };
+
+        String[] nouns = {
+                "dog",
+                "cat",
+                "human",
+                "planet",
+                "flower",
+                "ocean",
+                "fork",
+                "truck",
+                "mushroom",
+                "pizza"
+        };
+
+        randomNoun = randomElement(nouns);
+        randomAdj = randomElement(adjectives);
+        System.out.printf("Here is your server name:%n%s-%s", randomAdj, randomNoun);
+
+
+    }
+}
