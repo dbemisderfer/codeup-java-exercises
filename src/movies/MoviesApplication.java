7184cc556372723a2e3e87839e2bd50abb531d12
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-27 16:23:02 -0500

    partial completion of movie app exercise from arrays lesson

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,5 +1,5 @@
 package movies;
-
+import util.Input;
 import java.util.Arrays;
 
 public class MoviesApplication {
@@ -34,37 +34,40 @@ public class MoviesApplication {
             }
         } else if (userSelection == 1) {
             for (Movie movie : movieArray) {
-                System.out.println(movie.getName());
-                }
+                System.out.println(movie.getName() + " -- " + movie.getCategory());
+            }
         }
-
     }
 
     public static void main(String[] args) {
-//        System.out.println(MoviesArray.findAll());
+        System.out.println(MoviesArray.findAll());
 //        Movie movie2 = new Movie("Goodbye", "drama");
-//        System.out.println(Arrays.toString(MoviesArray.findAll()));
+        System.out.println(Arrays.toString(MoviesArray.findAll()));
 //        for (Movie movie : MoviesArray.findAll()) {
 //            System.out.println(movie);
 //        }
-//        Movie[] movieArray = MoviesArray.findAll();
-//////        for (int i = 0; i < movieArray.length; i++) {
-//////            System.out.println(movieArray[i].getName());
-//////            System.out.println(movieArray[i].getCategory());
-//////        }
+        Movie[] movieArray = MoviesArray.findAll();
+////        for (int i = 0; i < movieArray.length; i++) {
+////            System.out.println(movieArray[i].getName());
+////            System.out.println(movieArray[i].getCategory());
+////        }
 //        for (Movie movie : movieArray) {
 ////            System.out.println(movie[0]); // returns memory location
 ////            System.out.println(Arrays.toString(movie));
-//            System.out.println(movie.getName());
+////            System.out.println(movie.getName());
 ////            System.out.println(movie.getCategory());
-////            if (movie.getCategory().equals("scifi")) {
-////                System.out.println(movie.getName());
+//            if (movie.getCategory().equals("scifi")) {
+//                System.out.println(movie.getName());
 //            }
 //
 //        }
 //        System.out.println(Arrays.toString(movieArray));
 //        System.out.println(Arrays.toString());
 //        System.out.println(getMovieCategory(movieArray, 5));
-//        getMovieCategory(movieArray, 5);
+//        getMovieCategory(movieArray, 5));
+        System.out.println("Select a number between 0 and 5");
+        Input input = new Input();
+        int selectionNum = input.getInt(0, 5);
+        getMovieCategory(movieArray, selectionNum);
     }
 }
