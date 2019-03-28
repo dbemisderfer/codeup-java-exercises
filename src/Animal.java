243a15361e187446b2435ef5d467a74cc6be0a84
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-28 13:44:57 -0500

    git add src/Cat.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,31 @@
+import java.util.Date;
+
 public class Animal {
+    public String eyeColor;
+    public int age;
+    public int weight;
+    public String species;
+    public Date birthdate;
+
+    public Animal() {
+        this.birthdate = new Date();
+    }
+
+    public void eat(String food) {
+        System.out.printf("Nom, nom, nom. The %s eats the %s%n", this.species, food);
+    }
+
+    public static void sleep() {
+        System.out.println("Zzzz...Zzzzz...Zzzzzz.");
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal cat4 = new Cat();
+//        animal.eat();
+        animal.sleep();
+//        Animal.eat();
+        Animal.sleep();
+        cat4.eat("catfood");
+    }
 }
--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,20 @@
-public class Cat {
+public class Cat extends Animal {
+
+//    public Cat() {
+//        this.species = "cat";
+//    }
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Animal cat2 = new Animal();
+        Animal cat3 = new Cat();
+//        cat.eat();
+        cat.sleep();
+//        Cat.eat();
+        Cat.sleep();
+//        cat2.eat();
+//        Animal.eat();
+        cat3.sleep();
+//        cat.species = "cat";
+//        cat.eat();
+    }
 }
