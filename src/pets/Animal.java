d2db23bcdde1ce7792910e62b18484efbab6e37c
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-28 20:43:46 -0500

    inheritance and polymorphism practice

--- src/pets/Animal.java
+++ src/pets/Animal.java
@@ -1,3 +1,6 @@
+package pets;
+
+import java.util.Arrays;
 import java.util.Date;
 
 public class Animal {
@@ -6,27 +9,44 @@ public class Animal {
     public int weight;
     public String species;
     public Date birthdate;
+    public String name;
 
-    public Animal() {
+    public Animal(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
         this.birthdate = new Date();
     }
 
-    public void eat(String food) {
-        System.out.printf("Nom, nom, nom. The %s eats the %s%n", this.species, food);
+//    public String getName() {
+//        return name;
+//    }
+
+//    public int getWeight() {
+//        return weight;
+//    }
+
+    public Animal() {
+
     }
 
-    public static void sleep() {
+    public void sleep() {
         System.out.println("Zzzz...Zzzzz...Zzzzzz.");
     }
 
+    public void eat(String food) {
+        System.out.printf("Nom, nom, nom. The %s lb. %s eats the %s.%n", this.weight, this.species, food);
+    }
+
     public static void main(String[] args) {
-        Animal animal = new Animal();
-        Animal cat4 = new Cat();
+        int[] numbers = {1, 2, 3};
+        System.out.println(Arrays.toString(numbers));
+//        Animal animal = new Animal("Howdy", 6);
+//        Animal cat4 = new Cat("Tabby", 5);
 //        animal.eat();
-        animal.sleep();
-//        Animal.eat();
-        Animal.sleep();
-        cat4.eat("catfood");
+//        animal.sleep();
+//        pets.Animal.eat();
+//        Animal.sleep();
+//        cat4.eat("catfood");
 //
     }
 }
--- src/pets/Cat.java
+++ src/pets/Cat.java
@@ -1,21 +1,61 @@
-public class Cat extends Animal {
+package pets;
 
-//    public Cat() {
-//        this.species = "cat";
-//    }
+import java.util.Arrays;
+
+public class Cat extends Animal {
+//    public String name;
+
+    public Cat(String name, int weight) {
+//        this.name = name;
+        super(name, weight);
+        this.species = "cat";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void sleep() {
+        System.out.println("Mice.....Mmmmmmmm, yummy.");
+    }
+
+    public void eat(String food) {
+        System.out.printf("Meow, meow. %s eats the %s.%n", name, food);
+    }
+
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        Animal cat2 = new Animal();
-        Animal cat3 = new Cat();
-//        cat.eat();
-        cat.sleep();
-//        Cat.eat();
-        Cat.sleep();
-//        cat2.eat();
-//        Animal.eat();
-        cat3.sleep();
-//        cat.species = "cat";
-//        cat.eat();
+        Cat cat = new Cat("Kitty", 6);
+//        Animal cat2 = new Animal("Henry", 5);
+//        Animal cat3 = new Cat("Boots", 4);
+        Cat cat_x = new Cat("Willy", 4);
+        Cat cat_y = new Cat("Sam", 5);
+        Cat[] cats = {cat, cat_x, cat_y};
+//        System.out.println(Arrays.toString(cats));
+        for (Cat kat : cats) {
+            System.out.println("Name: " + kat.getName());
+            System.out.println("Weight: " + kat.getWeight());
+        }
+
+//        cat.sleep();             // Cat method
+//        cat.eat("hotdog"); // Cat method
+//        System.out.println();
+//        Cat.eat("tuna");
+//        Cat.sleep();
+//        cat2.species = "cat";
+//        cat2.weight = 5;
+//        cat2.sleep();               // Animal method
+//        cat2.eat("mackerel"); // Animal method
+//        System.out.println();
+//        pets.Animal.eat("canary");
+//        cat2.eat("fish");
+//        cat3.sleep();               // Animal method (when static)
+//        cat3.eat("mouse");     // Cat method
+
+
 //
     }
 }
