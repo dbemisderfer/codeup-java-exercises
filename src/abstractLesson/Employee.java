c0a031115051d4ec0889baa628a1bb3d05c9298a
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-29 15:31:10 -0500

    minor edit

--- src/abstractLesson/Employee.java
+++ src/abstractLesson/Employee.java
@@ -3,6 +3,7 @@ package abstractLesson;
 abstract class Employee {
     protected String name;
     protected String department;
+
     public Employee(String name, String department) {
         this.name = name;
         this.department = department;
