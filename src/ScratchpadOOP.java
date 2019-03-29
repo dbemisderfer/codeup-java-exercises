38fb571db1e300586fff81cc37d39fca42c1e70b
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-29 12:29:59 -0500

    minor edits

--- src/ScratchpadOOP.java
+++ src/ScratchpadOOP.java
@@ -1,2 +1,30 @@
 public class ScratchpadOOP {
+    class Student {
+        public String name;
+        public String cohort;
+
+////////////////////////////////////////////////
+// Can do this to create constructor, or..... //
+////////////////////////////////////////////////
+//        public Student(String name) {             //////////////////////////
+//            this.name = name;           ///// FIRST CONSTRUCTOR: METHOD ONE
+//            cohort = "Unassigned";                //////////////////////////
+//        }
+        
+////////////////////////////////////////////////////
+// .....call on the 2nd constructor's properties  //
+////////////////////////////////////////////////////
+        public Student(String name) {                       //////////////////////////
+            this(name, "Unassigned");      ///// FIRST CONSTRUCTOR: METHOD TWO
+        }                                                   //////////////////////////
+
+        public Student(String name, String cohort) {    //////////////////
+            this.name = name;               ///// SECOND CONSTRUCTOR /////
+            this.cohort = cohort;                       //////////////////
+        }
+
+        public String sayHello() {
+            return "Hello from " + this.name + "!";
+        }
+    }
 }
