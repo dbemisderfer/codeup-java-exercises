e8784448c1e1618d5cba2136a321b46e495f4fb5
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-29 11:37:38 -0500

    partial completion of more shapes exercise from interfaces and abstract classes lesson (up to exercise 4)

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -2,25 +2,25 @@ package shapes;
 
 public class Rectangle {
 
-    protected int length;
-    protected int width;
-
-    public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
-    }
-
-//    public Rectangle() {
+//    protected int length;
+//    protected int width;
 //
+//    public Rectangle(int length, int width) {
+//        this.length = length;
+//        this.width = width;
 //    }
-
-    public int getArea() {
-        return this.length * this.width;
-    }
-
-    public int getPerimeter() {
-        return (2 * this.length) + (2 * this.width);
-    }
+//
+////    public Rectangle() {
+////
+////    }
+//
+//    public int getArea() {
+//        return this.length * this.width;
+//    }
+//
+//    public int getPerimeter() {
+//        return (2 * this.length) + (2 * this.width);
+//    }
 
     public static void main(String[] args) {
     }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,21 +2,23 @@ package shapes;
 
 public class Square extends Rectangle {
 
-//    protected int side;
-
-    public Square(int side) {
-        super(side, side);
-//        this.side = side;
-
-    }
-
-    public int getArea() {
-        return (this.length * this.length);
-    }
-
-    public int getPerimeter() {
-        return 4 * this.length;
-    }
+////    protected int side;   // If using "side" as a variable in the "get" methods below
+//
+//    public Square(int side) {
+//        super(side, side);
+////        this.side = side;  // If using "side" as a variable in the "get" methods below
+//
+//    }
+//
+//    public int getArea() {
+//        return (this.length * this.length);
+////        return (side * side);
+//    }
+//
+//    public int getPerimeter() {
+//        return 4 * this.length;
+////        return 4 * side;
+//    }
 
     public static void main(String[] args) {
 
