cabef73825d02b5f74c6194d04b2289108b257ac
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-02 13:49:54 -0500

    trying to figure out nextLine vs next

--- src/ScratchPadScanner.java
+++ src/ScratchPadScanner.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class ScratchPadScanner {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Enter something: ");
+        String userInput = scanner.nextLine();
+//        userInput = scanner.next();
+
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+    }
+
 }
