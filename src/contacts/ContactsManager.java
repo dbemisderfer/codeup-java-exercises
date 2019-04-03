e4bce06235a50965d1e4838c9905d5181cef75b1
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-03 08:59:52 -0500

    added ability to write names and phone numbers to contact list

--- src/contacts/ContactsManager.java
+++ src/contacts/ContactsManager.java
@@ -1,23 +1,21 @@
 package contacts;
 import util.Input;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ContactsManager {
     static Input input;
+    static String directory = "src/contacts/data";
+    static String filename = "contactlist.txt";
 
-    public static void addContact() {
-        System.out.println("Enter first name: ");
-        String placeholder = input.getString();
-        String firstName = input.getString();
-        System.out.println("Enter last name: ");
-        String lastName = input.getString();
-        System.out.println("Enter phone number: ");
-        String phoneNum = input.getString();
-        System.out.printf("%s %s %s%n", firstName, lastName, phoneNum);
-    }
-    public static void main(String[] args) {
-//        Contact contact1 = new Contact("Marty", "Henson", "1231231234");
-//        System.out.printf("%s %s %s%n", contact1.getFirstName(), contact1.getLastName(), contact1.getPhone());
-        input = new Input();
-
+    public static void selectionList() {
         System.out.println("1. View contacts.\n" +
                 "2. Add a new contact.\n" +
                 "3. Search a contact by name.\n" +
@@ -29,7 +27,12 @@ public class ContactsManager {
             case 1 :
                 break;
             case 2 :
-                addContact();
+                try {
+                    addContact();
+                } catch (IOException ioe){
+                    ioe.printStackTrace();
+                }
+
                 break;
             case 3 :
                 break;
@@ -39,5 +42,37 @@ public class ContactsManager {
 //                System.exit(0);
                 return;
         }
+    }
+
+    public static void addContact() throws IOException {
+        System.out.println("Enter first name: ");
+        String placeholder = input.getString();
+        String firstName = input.getString();
+        System.out.println("Enter last name: ");
+        String lastName = input.getString();
+        System.out.println("Enter phone number: ");
+        String phoneNum = input.getString();
+        System.out.printf("%s %s %s%n", firstName, lastName, phoneNum);
+        Path contactDirectory = Paths.get(directory);
+        Path contactFile = Paths.get(directory, filename);
+        if (Files.notExists(contactDirectory)) {
+            Files.createDirectories(contactDirectory);
+        }
+        if (Files.notExists(contactFile)) {
+            Files.createFile(contactFile);
+        }
+//        List<String> lines = Files.readAllLines()
+        Files.write(
+                Paths.get(directory, filename),
+                Arrays.asList(firstName, lastName, phoneNum),
+                StandardOpenOption.APPEND
+        );
+    }
+    public static void main(String[] args) {
+//        Contact contact1 = new Contact("Marty", "Henson", "1231231234");
+//        System.out.printf("%s %s %s%n", contact1.getFirstName(), contact1.getLastName(), contact1.getPhone());
+        input = new Input();
+        selectionList();
+
     }
 }
