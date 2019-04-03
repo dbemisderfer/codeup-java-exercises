df5e9b1f4809bda3977378ad7b2d9d4e57355a27
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-03 11:00:56 -0500

    added ability to add contacts

--- src/contacts/ContactsManager.java
+++ src/contacts/ContactsManager.java
@@ -15,38 +15,9 @@ public class ContactsManager {
     static String directory = "src/contacts/data";
     static String filename = "contactlist.txt";
 
-    public static void selectionList() {
-        System.out.println("1. View contacts.\n" +
-                "2. Add a new contact.\n" +
-                "3. Search a contact by name.\n" +
-                "4. Delete an existing contact.\n" +
-                "5. Exit.\n" +
-                "Enter an option (1, 2, 3, 4 or 5): ");
-        int choose = input.getInt();
-        switch (choose) {
-            case 1 :
-                break;
-            case 2 :
-                try {
-                    addContact();
-                } catch (IOException ioe){
-                    ioe.printStackTrace();
-                }
-
-                break;
-            case 3 :
-                break;
-            case 4 :
-                break;
-            case 5 :
-//                System.exit(0);
-                return;
-        }
-    }
-
     public static void addContact() throws IOException {
         System.out.println("Enter first name: ");
-        String placeholder = input.getString();
+//        String placeholder = input.getString();
         String firstName = input.getString();
         System.out.println("Enter last name: ");
         String lastName = input.getString();
@@ -72,7 +43,32 @@ public class ContactsManager {
 //        Contact contact1 = new Contact("Marty", "Henson", "1231231234");
 //        System.out.printf("%s %s %s%n", contact1.getFirstName(), contact1.getLastName(), contact1.getPhone());
         input = new Input();
-        selectionList();
 
+        System.out.println("1. View contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n" +
+                "Enter an option (1, 2, 3, 4 or 5): ");
+        int choose = input.getInt();
+        switch (choose) {
+            case 1 :
+                break;
+            case 2 :
+                try {
+                    addContact();
+            } catch (IOException ioe){
+                    ioe.printStackTrace();
+            }
+
+                break;
+            case 3 :
+                break;
+            case 4 :
+                break;
+            case 5 :
+//                System.exit(0);
+                return;
+        }
     }
 }
